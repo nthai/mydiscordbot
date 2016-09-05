8d554c4561dbf0d31f0aca8d829b2543d5c370ad
nguyentuanhai <nthaitrx@gmail.com>
2016-09-05 15:54:58 +0200

    Changed client to bot.

--- sample.py
+++ sample.py
@@ -1,27 +1,40 @@
+from discord.ext import commands
 import discord
+import random
 import asyncio
+
+description = "A Józsiarcúak csatorna dicsőséges botja."
+
+prefix = ['!']
+formatter = commands.HelpFormatter(show_check_failure = False)
+bot = commands.Bot(command_prefix=prefix, description=description)
+
 
 client = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as...')
-    print(client.user.name)
-    print(client.user.id)
+    print('Username: ' + bot.user.name)
+    print('ID: ' + bot.user.id)
     print('-----')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if(message.content == '!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit = 100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif(message.content == '!whoami'):
-        await client.send_message(message.channel, 'You are {}.'.format(message.author))
+    if message.author.bot:
+        return
+    await bot.process_commands(message)
+
+@bot.command()
+async def kocka(dice : int):
+    """Random egész szám generátor"""
+    try:
+        result = str(random.randint(1, dice))
+    except Exception:
+        await bot.say('Error during command: kocka')
+        return
+    await bot.say('Dobás ' + str(dice) + ' oldalú kockával: ' + result)
 
 with open('token.tk', mode='r') as input:
     token = input.readline().strip()
-    client.run(token)
+    bot.run(token)
